0f10fa567dd4fb4015c6107ffe22540941f27e86
Jacob Lista <jlista@solutechnology.com>
2024-12-07 23:48:30 -0500

    Day 7 finished

---
+++ day7.py
@@ -0,0 +1,44 @@
+from library import *
+import math
+import copy
+
+
+def try_op(cur, nums, index, op, goal):
+    
+    if index == len(nums):
+        if cur == goal:
+            return True
+        return False
+
+    if index == 0:
+        val = nums[0]
+    elif op == "*":
+        val = cur * nums[index]
+    elif op == '+':
+        val = cur + nums[index]
+    elif op == "|":
+        val = int(f"{cur}{nums[index]}")
+
+    return try_op(val, nums, index+1, "+", goal) or \
+           try_op(val, nums, index+1, "*", goal) or \
+           try_op(val, nums, index+1, "|", goal)
+    
+
+with open("C:/Python/2024/day7input.txt", 'r') as file:
+
+    # Parse input 
+
+    lines = [line.strip().split(': ') for line in file]
+    results = [int(line[0]) for line in lines]
+    numlist = [[int(n) for n in line[1].split(' ')] for line in lines]
+
+    print("hi")
+
+    count = 0
+    for res, nums in zip(results, numlist):
+        valid = try_op(0, nums, 0, None, res)
+        if valid:
+            count += res
+
+    print(count)
+
